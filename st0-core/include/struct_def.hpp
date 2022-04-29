1fa04ed2a422f21938a41ebdb76683426c578a37
csl <3079625093@qq.com>
2022-04-29 20:02:16 +0800

    update

--- st0-core/include/struct_def.hpp
+++ st0-core/include/struct_def.hpp
@@ -10,6 +10,10 @@
 #ifndef STRUCT_DEFINE_H
 #define STRUCT_DEFINE_H
 
+#include "eigen3/Eigen/Dense"
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+
 namespace ns_st0 {
   /**
    * @brief a structure for camera' inner parameters' organization
@@ -18,6 +22,13 @@ namespace ns_st0 {
     float fx, fy, cx, cy;
     CameraInnerParam(const float &fx, const float &fy, const float &cx, const float &cy)
         : fx(fx), fy(fy), cx(cx), cy(cy) {}
+
+    inline Eigen::Matrix3f toEigenMatrix() const {
+      Eigen::Matrix3f K = Eigen::Matrix3f::Identity();
+      K(0, 0) = fx, K(1, 1) = fy;
+      K(0, 2) = cx, K(1, 2) = cy;
+      return K;
+    }
   };
 
   /**
@@ -39,6 +50,68 @@ namespace ns_st0 {
     NEAREST_NEIGHBOR,
     BILINEAR
   };
+
+  /**
+   * @brief get antisymmetric matrix from a vector
+   *
+   * @tparam Type the vaule type
+   * @param vec the vector
+   * @return Eigen::Matrix<Type, 3, 3> the antisymmetric matrix
+   */
+  template <typename Type>
+  Eigen::Matrix<Type, 3, 3> antisymmetric(const Eigen::Vector<Type, 3> &vec) {
+    Type x = vec(0), y = vec(1), z = vec(2);
+    Eigen::Matrix<Type, 3, 3> mat;
+    mat(0, 0) = 0, mat(0, 1) = -z, mat(0, 2) = y;
+    mat(1, 0) = z, mat(1, 1) = 0, mat(1, 2) = -x;
+    mat(2, 0) = -y, mat(2, 1) = x, mat(2, 2) = 0;
+    return mat;
+  }
+
+  /**
+   * @brief recovery the vector from a antisymmetric matrix
+   *
+   * @tparam Type the value type
+   * @param mat the antisymmetric matrix
+   * @return Eigen::Vector<Type, 3> the vector
+   */
+  template <typename Type>
+  Eigen::Vector<Type, 3> antisymmetric(const Eigen::Matrix<Type, 3, 3> &mat) {
+    Eigen::Vector<Type, 3> vec(-mat(1, 2), mat(0, 2), -mat(0, 1));
+    return vec;
+  }
+
+  /**
+   * @brief detect the key points anf match them
+   *
+   * @param img1 the first image
+   * @param img2 the second image
+   * @param kps1 the key points in first image
+   * @param kps2 the key points in second image
+   * @param match the matches
+   */
+  void detectAndMatch(cv::Mat img1, cv::Mat img2,
+                      std::vector<cv::KeyPoint> &kps1,
+                      std::vector<cv::KeyPoint> &kps2,
+                      std::vector<cv::DMatch> &match) {
+    cv::Mat descriptors1, descriptors2;
+
+    cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
+    cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
+    
+    // detector
+    detector->detect(img1, kps1);
+    detector->detect(img2, kps2);
+
+    // compute
+    descriptor->compute(img1, kps1, descriptors1);
+    descriptor->compute(img2, kps2, descriptors2);
+
+    // match
+    matcher->match(descriptors1, descriptors2, match);
+    return;
+  }
 }
 
 #endif
