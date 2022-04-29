1ce8435d92aaf04826734c8dd6096a69728135fe
csl <3079625093@qq.com>
2022-04-29 13:42:48 +0800

    update

--- st2-epipolar/src/include/epipolar.hpp
+++ st2-epipolar/src/include/epipolar.hpp
@@ -4,20 +4,48 @@
 #include "eigen3/Eigen/Dense"
 #include "opencv2/core.hpp"
 #include "struct_def.hpp"
+#include <algorithm>
 #include <iostream>
 #include <set>
 
 using namespace ns_st0;
 
 namespace ns_st2 {
+  /**
+   * @brief to get function matrix based on the epipolar constraints
+   *
+   * @param kps1 the keypoints in the first image
+   * @param kps2 the keypoints in the second image
+   * @param srcMatches the source matches, It can be matching data without preprocessing
+   * @param goodMatches the good matches that this algorithm return
+   * @param quantile the quantile to judge whether a match is an outlier
+   * @return Eigen::Matrix3f the function matrix
+   */
   static Eigen::Matrix3f solveEpipolar(
       const std::vector<cv::KeyPoint> &kps1,
       const std::vector<cv::KeyPoint> &kps2,
-      const std::vector<cv::DMatch> &matches,
+      const std::vector<cv::DMatch> &srcMatches,
       std::vector<cv::DMatch> *goodMatches = nullptr,
       float quantile = 3.8415) {
 
-    CV_Assert(matches.size() >= 8);
+    CV_Assert(srcMatches.size() >= 8);
+
+    // clean source data
+    std::vector<cv::DMatch> matches;
+    matches.reserve(0.5 * srcMatches.size());
+
+    auto minDisIter = std::min_element(
+        srcMatches.cbegin(), srcMatches.cend(),
+        [](const cv::DMatch &m1, const cv::DMatch &m2) {
+          return m1.distance < m2.distance;
+        });
+
+    for (int i = 0; i != srcMatches.size(); ++i) {
+      // filter bad matches
+      if (srcMatches.at(i).distance < std::max(30.0f, 2.0f * minDisIter->distance)) {
+        matches.push_back(srcMatches.at(i));
+      }
+    }
 
     // matrices for least square
     Eigen::MatrixXd matA(matches.size(), 8), vecl = -Eigen::VectorXd::Ones(matches.size());
@@ -91,16 +119,8 @@ namespace ns_st2 {
         // which means no outliers in current left matches
         break;
       } else {
-
-        std::cout << "max idx: " << maxIdx << std::endl;
-        std::cout << "max var: " << maxVar << std::endl;
-        std::cout << "outliers size: " << outliers.size() << std::endl;
-        std::cout << "left size: " << matches.size() - outliers.size() << std::endl;
-        std::cout << matX.transpose() << std::endl;
-        std::cout << std::endl;
-
+        // remove the outlier's affect
         matA.row(maxIdx).setZero();
-        vecl.row(maxIdx).setZero();
         outliers.insert(maxIdx);
       }
     }
@@ -114,10 +134,12 @@ namespace ns_st2 {
 
     if (goodMatches != nullptr) {
       goodMatches->clear();
+      goodMatches->resize(matches.size() - outliers.size());
+      int count = 0;
       for (int i = 0; i != matches.size(); ++i) {
         if (outliers.count(i) == 0) {
           // it's not a outliers
-          goodMatches->push_back(matches.at(i));
+          goodMatches->at(count++) = (matches.at(i));
         }
       }
     }
--- st2-epipolar/src/main.cpp
+++ st2-epipolar/src/main.cpp
@@ -1,3 +1,4 @@
+#include "artwork/timer/timer.h"
 #include "epipolar.hpp"
 #include "opencv2/opencv.hpp"
 #include <algorithm>
@@ -7,7 +8,7 @@ void detectAndMatch(cv::Mat img1, cv::Mat img2,
                     std::vector<cv::KeyPoint> &kps2,
                     std::vector<cv::DMatch> &match) {
 
-  cv::Mat descriptors1, ddescriptors2;
+  cv::Mat descriptors1, descriptors2;
 
   cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
   cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
@@ -17,9 +18,9 @@ void detectAndMatch(cv::Mat img1, cv::Mat img2,
   detector->detect(img2, kps2);
 
   descriptor->compute(img1, kps1, descriptors1);
-  descriptor->compute(img2, kps2, ddescriptors2);
+  descriptor->compute(img2, kps2, descriptors2);
 
-  matcher->match(descriptors1, ddescriptors2, match);
+  matcher->match(descriptors1, descriptors2, match);
   return;
 }
 
@@ -35,9 +36,17 @@ int main(int argc, char const *argv[]) {
 
   detectAndMatch(img1, img2, kps1, kps2, match);
 
+  ns_timer::Timer timer;
+
   {
     // for OpenCV
-    std::vector<cv::DMatch> matches;
+    std::cout << "---------------" << std::endl;
+    std::cout << "Solve By OpenCV" << std::endl;
+    std::cout << "---------------" << std::endl;
+
+    timer.re_start();
+
+    std::vector<cv::DMatch> goodMatches;
 
     double min_dist = std::min_element(match.cbegin(), match.cend(), [](const cv::DMatch &m1, const cv::DMatch &m2) {
                         return m1.distance < m2.distance;
@@ -45,52 +54,101 @@ int main(int argc, char const *argv[]) {
 
     for (int i = 0; i < match.size(); i++) {
       if (match[i].distance < std::max(2.0 * min_dist, 30.0)) {
-        matches.push_back(match[i]);
+        goodMatches.push_back(match[i]);
       }
     }
 
-    // cv::Mat matchImg;
-    // cv::drawMatches(img1, kps1, img2, kps2, matches, matchImg);
-    // cv::imshow("match", matchImg);
-    // cv::waitKey(0);
-    // cv::imwrite("../img/select-matches-opencv.png", matchImg);
-
     std::vector<cv::Point2f> pts1;
     std::vector<cv::Point2f> pts2;
-    for (int i = 0; i < (int)matches.size(); i++) {
-      pts1.push_back(kps1[matches[i].queryIdx].pt);
-      pts2.push_back(kps2[matches[i].trainIdx].pt);
+    for (int i = 0; i < goodMatches.size(); i++) {
+      pts1.push_back(kps1[goodMatches[i].queryIdx].pt);
+      pts2.push_back(kps2[goodMatches[i].trainIdx].pt);
     }
 
     auto f = cv::findFundamentalMat(pts1, pts2, cv::FM_8POINT);
 
-    std::cout << "---------------" << std::endl;
-    std::cout << "Solve By OpenCV" << std::endl;
-    std::cout << "---------------" << std::endl;
-    std::cout << "matched points: " << matches.size() << std::endl;
-    std::cout << f << std::endl;
+    std::cout << timer.last_elapsed("cost time") << std::endl;
+    std::cout << "matched points: " << goodMatches.size() << std::endl;
+    std::cout << "fun matrix:\n"
+              << f << std::endl;
 
-    // debug
-    cv::Mat p1 = (cv::Mat_<double>(3, 1) << kps1.at(matches.front().queryIdx).pt.x, kps1.at(matches.front().queryIdx).pt.y, 1.0);
-    cv::Mat p2 = (cv::Mat_<double>(3, 1) << kps2.at(matches.front().trainIdx).pt.x, kps2.at(matches.front().trainIdx).pt.y, 1.0);
-    cv::Mat temp = p2.t() * f;
-    double a = temp.at<double>(0, 0);
-    double b = temp.at<double>(0, 1);
-    double c = temp.at<double>(0, 2);
-    std::cout << "p1: " << p1.t() << std::endl;
-    std::cout << "p2: " << p2.t() << std::endl;
-    std::cout << "'a,b,c': " << a << ", " << b << ", " << c << std::endl;
-    double num = a * kps1.at(matches.front().queryIdx).pt.x + b * kps1.at(matches.front().queryIdx).pt.y + c;
-    std::cout << "'num': " << num << std::endl;
-    std::cout << "den: " << a * a + b * b << std::endl;
-    std::cout << "'num*num/den': " << num * num / (a * a + b * b) << std::endl;
-    std::cout << "------------------------" << std::endl;
-    auto f2 = ns_st2::solveEpipolar(kps1, kps2, matches);
-    std::cout << f2 << std::endl;
+    float mean = 0.0, sigma = 0.0, errorMeanNorm = 0.0;
+    for (int i = 0; i != goodMatches.size(); ++i) {
+      const auto &match = goodMatches.at(i);
+      cv::Mat p1 = (cv::Mat_<double>(3, 1) << kps1.at(match.queryIdx).pt.x, kps1.at(match.queryIdx).pt.y, 1.0);
+      cv::Mat p2 = (cv::Mat_<double>(3, 1) << kps2.at(match.trainIdx).pt.x, kps2.at(match.trainIdx).pt.y, 1.0);
+      cv::Mat temp = p2.t() * f * p1;
+      mean += temp.at<double>(0, 0);
+      errorMeanNorm += std::abs(temp.at<double>(0, 0));
+    }
+    mean /= goodMatches.size();
+    errorMeanNorm /= goodMatches.size();
+    std::cout << "mean: " << mean << std::endl;
+    std::cout << "errorMeanNorm: " << errorMeanNorm << std::endl;
+
+    for (int i = 0; i != goodMatches.size(); ++i) {
+      const auto &match = goodMatches.at(i);
+      cv::Mat p1 = (cv::Mat_<double>(3, 1) << kps1.at(match.queryIdx).pt.x, kps1.at(match.queryIdx).pt.y, 1.0);
+      cv::Mat p2 = (cv::Mat_<double>(3, 1) << kps2.at(match.trainIdx).pt.x, kps2.at(match.trainIdx).pt.y, 1.0);
+      cv::Mat temp = p2.t() * f * p1;
+      sigma += std::pow(mean - temp.at<double>(0, 0), 2.0);
+    }
+    sigma /= goodMatches.size() - 1;
+    std::cout << "sigma: " << std::sqrt(sigma) << std::endl;
+
+    cv::Mat matchImg;
+    cv::drawMatches(img1, kps1, img2, kps2, goodMatches, matchImg);
+    cv::imshow("match", matchImg);
+    cv::waitKey(0);
+    cv::imwrite("../img/select-matches-opencv.png", matchImg);
   }
+
   {
     // for our method
-    
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Solve By Ourself" << std::endl;
+    std::cout << "---------------" << std::endl;
+
+    std::vector<cv::DMatch> goodMatches;
+    timer.re_start();
+    auto f = ns_st2::solveEpipolar(kps1, kps2, match, &goodMatches);
+    std::cout << timer.last_elapsed("cost time") << std::endl;
+    std::cout << "matched points: " << goodMatches.size() << std::endl;
+    std::cout << "fun matrix:\n"
+              << f << std::endl;
+
+    float mean = 0.0, sigma = 0.0, errorMeanNorm = 0.0;
+    for (int i = 0; i != goodMatches.size(); ++i) {
+      const auto &match = goodMatches.at(i);
+      float u1 = kps1.at(match.queryIdx).pt.x, v1 = kps1.at(match.queryIdx).pt.y;
+      float u2 = kps2.at(match.trainIdx).pt.x, v2 = kps2.at(match.trainIdx).pt.y;
+      Eigen::Vector3f p1(u1, v1, 1.0f);
+      Eigen::Vector3f p2(u2, v2, 1.0f);
+      mean += (p2.transpose() * f * p1)(0, 0);
+      errorMeanNorm += std::abs((p2.transpose() * f * p1)(0, 0));
+    }
+    mean /= goodMatches.size();
+    errorMeanNorm /= goodMatches.size();
+    std::cout << "mean: " << mean << std::endl;
+    std::cout << "errorMeanNorm: " << errorMeanNorm << std::endl;
+
+    for (int i = 0; i != goodMatches.size(); ++i) {
+      const auto &match = goodMatches.at(i);
+      float u1 = kps1.at(match.queryIdx).pt.x, v1 = kps1.at(match.queryIdx).pt.y;
+      float u2 = kps2.at(match.trainIdx).pt.x, v2 = kps2.at(match.trainIdx).pt.y;
+      Eigen::Vector3f p1(u1, v1, 1.0f);
+      Eigen::Vector3f p2(u2, v2, 1.0f);
+      sigma += std::pow(mean - (p2.transpose() * f * p1)(0, 0), 2.0);
+    }
+    sigma /= goodMatches.size() - 1;
+    std::cout << "sigma: " << std::sqrt(sigma) << std::endl;
+
+    cv::Mat matchImg;
+    cv::drawMatches(img1, kps1, img2, kps2, goodMatches, matchImg);
+    cv::imshow("match", matchImg);
+    cv::waitKey(0);
+    cv::imwrite("../img/select-matches-ourself.png", matchImg);
   }
   return 0;
 }
