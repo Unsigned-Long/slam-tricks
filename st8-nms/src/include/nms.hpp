0705aeafd4a8912404a76f17e02b97ef90537335
csl <3079625093@qq.com>
2022-06-15 17:08:38 +0800

    add nms test

---
+++ st8-nms/src/include/nms.hpp
@@ -0,0 +1,82 @@
+#ifndef NMS_HPP
+#define NMS_HPP
+
+#define FORMAT_VECTOR
+#include "artwork/logger/logger.h"
+#include "opencv2/opencv.hpp"
+#include <cmath>
+
+namespace ns_st8 {
+  static std::vector<ushort> nms1d(const std::vector<float> &vals, const ushort hws) {
+    std::vector<ushort> max;
+    for (int i = hws; i < vals.size() - 2 * hws;) {
+      float maxVal = vals[i];
+      ushort maxIdx = i;
+      for (int j = i; j < i + hws + 1; ++j) {
+        if (vals[j] > vals[i]) {
+          maxIdx = j;
+          maxVal = vals[j];
+        }
+      }
+      bool findMax = true, hasLower = false;
+      for (int k = maxIdx - hws; k < maxIdx + hws + 1; ++k) {
+        if (k == maxIdx) {
+          continue;
+        }
+        if (vals[k] > maxVal) {
+          findMax = false;
+          break;
+        } else if (vals[k] < maxVal) {
+          hasLower = true;
+        }
+      }
+      if (findMax && hasLower) {
+        max.push_back(maxIdx);
+        i = maxIdx + hws + 1;
+      } else {
+        i += 1;
+      }
+    }
+    return max;
+  }
+  static std::vector<cv::Point2i> nms2d(const cv::Mat img, const ushort hws) {
+    std::vector<cv::Point2i> max;
+    int rows = img.rows, cols = img.cols;
+    for (int i = hws; i < rows - 2 * hws; i += hws + 1) {
+      for (int j = hws; j < rows - 2 * hws; j += hws + 1) {
+        uchar maxVal = img.at<uchar>(i, j);
+        cv::Point2i maxIdx(j, i);
+        for (int r = i; r < i + hws + 1; ++r) {
+          for (int c = j; c < j + hws + 1; ++c) {
+            uchar val = img.at<uchar>(r, c);
+            if (val > maxVal) {
+              maxVal = val;
+              maxIdx = cv::Point2i(c, r);
+            }
+          }
+        }
+        bool findMax = true, hasLower = false;
+        for (int r = maxIdx.y - hws; r < maxIdx.y + hws + 1; ++r) {
+          for (int c = maxIdx.x - hws; c < maxIdx.x + hws + 1; ++c) {
+            if (r == maxIdx.y && c == maxIdx.x) {
+              continue;
+            }
+            uchar val = img.at<uchar>(r, c);
+            if (val > maxVal) {
+              findMax = false;
+              break;
+            } else if (val < maxVal) {
+              hasLower = true;
+            }
+          }
+        }
+        if (findMax && hasLower) {
+          max.push_back(maxIdx);
+        }
+      }
+    }
+    return max;
+  }
+} // namespace ns_st7
+
+#endif
---
+++ st8-nms/src/main.cpp
@@ -0,0 +1,84 @@
+#include "artwork/geometry/point.hpp"
+#include "nms.hpp"
+#include <random>
+
+float gauss(float x) {
+  return std::pow(M_E, -x * x / 16);
+}
+float gauss2(float x) {
+  return std::pow(M_E, -x * x / 100);
+}
+
+void test_nms1d() {
+  std::vector<float> vals{};
+  for (int i = 0; i != 20; ++i) {
+    float dis1 = std::abs(7 - i);
+    float dis2 = std::abs(16 - i);
+    vals.push_back(gauss(dis1) + gauss(dis2));
+  }
+  LOG_VAR(vals);
+  LOG_VAR(ns_st8::nms1d(vals, 3));
+  std::default_random_engine e;
+  std::uniform_real_distribution<float> u(0.0f, 1.0f);
+  for (int i = 0; i != 20; ++i) {
+    vals[i] = u(e);
+  }
+  LOG_VAR(vals);
+  LOG_VAR(ns_st8::nms1d(vals, 3));
+}
+
+void test_nms2d() {
+  {
+    auto ps = ns_geo::PointSet2i::randomGenerator(20, 0, 199, 0, 199);
+    cv::Mat img(200, 200, CV_8UC1, cv::Scalar(0));
+    for (int i = 0; i != img.rows; ++i) {
+      for (int j = 0; j != img.cols; ++j) {
+        for (const auto &p : ps) {
+          float dis = ns_geo::distance(p, ns_geo::Point2i(i, j));
+          img.at<uchar>(i, j) += (uchar)(150.0f * gauss2(dis));
+        }
+      }
+    }
+    auto max = ns_st8::nms2d(img, 2);
+    LOG_VAR(max);
+
+    cv::Mat color;
+    cv::cvtColor(img, color, cv::COLOR_GRAY2BGR);
+    for (const auto &elem : max) {
+      cv::drawMarker(color, elem, cv::Scalar(0, 0, 255), cv::MarkerTypes::MARKER_CROSS, 5, 1);
+    }
+    cv::namedWindow("win", cv::WindowFlags::WINDOW_FREERATIO);
+    cv::imshow("win", color);
+    cv::waitKey(0);
+    cv::imwrite("../img/norm_nms2d_2.png", color);
+    cv::imwrite("../img/norm_nms2d_src.png", img);
+  }
+  {
+    cv::Mat img(20, 20, CV_8UC1);
+    std::default_random_engine e;
+    std::normal_distribution<float> n(127, 30);
+    for (int i = 0; i != img.rows; ++i) {
+      for (int j = 0; j != img.cols; ++j) {
+        img.at<uchar>(i, j) = n(e);
+      }
+    }
+    auto max = ns_st8::nms2d(img, 2);
+    LOG_VAR(max);
+
+    cv::Mat color;
+    cv::cvtColor(img, color, cv::COLOR_GRAY2BGR);
+    for (const auto &elem : max) {
+      color.at<cv::Vec3b>(elem) = cv::Vec3b(0, 0, 255);
+    }
+    cv::namedWindow("win", cv::WindowFlags::WINDOW_FREERATIO);
+    cv::imshow("win", color);
+    cv::waitKey(0);
+    cv::imwrite("../img/nms2d_2.png", color);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  // test_nms1d();
+  test_nms2d();
+  return 0;
+}
